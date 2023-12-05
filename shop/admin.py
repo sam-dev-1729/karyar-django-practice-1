998d33aa818974b06c15850bdcd928725786b03a
sam-dev-1729 <sam.dev.1729@gmail.com>
2023-12-05 12:22:12 +0330

    add models

--- shop/admin.py
+++ shop/admin.py
@@ -1,1 +1,37 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Category, Orders, Products, Sellers
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_display_links = ["name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    list_display_links = ["id", "name"]
+    list_filter = ["name", "price", "brand"]
+    search_fields = ["name", "price", "brand", "category"]
+
+
+class SellersAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "phone"]
+    list_display_links = ["id", "name"]
+    list_filter = ["name", "phone"]
+    search_fields = ["name"]
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ["id", "owner"]
+    list_display_links = ["id", "owner"]
+    list_filter = ["owner", "date"]
+    search_fields = ["owner", "date"]
+
+
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Sellers, SellersAdmin)
+admin.site.register(Orders, OrdersAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,1 +1,62 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
 # Create your models here.
+class Category(models.Model):
+    name: models.CharField = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name: models.CharField = models.CharField(max_length=150)
+    brand: models.CharField = models.CharField(max_length=150)
+    price: models.PositiveBigIntegerField = models.PositiveBigIntegerField()
+    created_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+    updated_at: models.DateTimeField = models.DateTimeField(
+        blank=True, null=True
+    )
+    category: models.ManyToManyField = models.ManyToManyField(Category)
+    seller: models.ForeignKey = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+
+    # seller: models.ForeignKey = models.ForeignKey(
+    #     Seller, on_delete=models.CASCADE
+    # )
+    def __str__(self) -> str:
+        return self.name
+
+
+class Sellers(models.Model):
+    name: models.CharField = models.CharField(max_length=150)
+    address: models.TextField = models.TextField()
+    phone: models.CharField = models.CharField(max_length=11)
+    banck_account: models.CharField = models.CharField(max_length=100)
+    user: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
+    # products = models.ManyToManyField(Products)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Orders(models.Model):
+    owner: models.ForeignKey = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    products: models.ForeignKey = models.ForeignKey(
+        Products,
+        on_delete=models.CASCADE,
+    )
+    quantity: models.IntegerField = models.IntegerField()
+    price: models.PositiveBigIntegerField = models.PositiveBigIntegerField()
+    description: models.TextField = models.TextField()
+    date: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Order"
